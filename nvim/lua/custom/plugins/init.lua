12d311f97e12fdf4ef59bdf4dde03188529c5046
asahtik <asahtik@gmail.com>
2023-10-02 09:30:11 +0200

    Search git files recursive

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -33,4 +33,10 @@ vim.keymap.set("n", "<leader>oi", "i<CR><Esc>")
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', function()
+	builtin.git_files({ recurse_submodules = true, use_git_root = true })
+end)
+
+
 return {}
