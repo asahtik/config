5054db671eb4aa4abd25f11da9d8731addf482a8
asahtik <asahtik@gmail.com>
2023-10-02 15:10:24 +0200

    Minor changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -522,8 +522,7 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-o>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-o>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
